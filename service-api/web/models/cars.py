eaf2d6e6b8bf9e7d866b7acd9b6f617d157fb919
Thanawat Udchachon <cdnoob@gmail.com>
2024-07-08 11:10:08 +0700

    migrate

--- service-api/web/models/cars.py
+++ service-api/web/models/cars.py
@@ -4,7 +4,7 @@ from web.models.option import *
 class Cars(models.Model):
     title = models.CharField(max_length=64, null=True, blank=True)
     type = models.CharField(max_length=64, null=True, blank=True)
-    capacity = models.IntegerField(max_length=64, null=True, blank=True)
+    capacity = models.IntegerField(null=True, blank=True)   #'max_length' is ignored when used with IntegerField
     license = models.CharField(max_length=64, null=True, blank=True,unique=True)
 
     def __str__(self):
